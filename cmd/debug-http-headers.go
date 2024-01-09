af90d0359008659b4401244dffff8296186779a5
Petr Haton <petr.haton@unicov-net.com>
2024-01-09 19:27:22 +0100

    Debugger HTTP hlavicek

---
+++ cmd/debug-http-headers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Získání všech HTTP hlaviček z požadavku
+	headers := r.Header
+
+	// Vytvoření řetězce obsahujícího všechny hlavičky
+	var headerStr string
+	for key, values := range headers {
+		for _, value := range values {
+			headerStr += fmt.Sprintf("%s: %s\n", key, value)
+		}
+	}
+
+	// Odeslání odpovědi s hlavičkami klientovi
+	fmt.Fprintf(w, "%s", headerStr)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Clkoud-for-You/debugger-pod
+
+go 1.21.5
